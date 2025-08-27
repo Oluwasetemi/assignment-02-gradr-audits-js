61a3a06584eb646f3eef57fa220731275b5c174a
oluwasetemi ojo <setemiojo@gmail.com>
2025-08-27 18:55:19 -0500

    Rename test function to use evenNumbersWithin and update test description for clarity

--- ch-2/test.js
+++ ch-2/test.js
@@ -1,13 +1,13 @@
-import { countEvenNumbersWithin } from '../src/assignment.js';
+import { evenNumbersWithin } from '../src/assignment.js';
 import axios from 'axios';
 
-const maybe = countEvenNumbersWithin === undefined ? test.skip : test;
+const maybe = evenNumbersWithin === undefined ? test.skip : test;
 
-maybe('Challenge - 2 : countEvenNumbersWithin can count, sum, arrayOfEvenNumbers from 1 to destination', async () => {
+maybe('Challenge - 2 : evenNumbersWithin can count, sum, arrayOfEvenNumbers from 0 to destination', async () => {
     const URL = 'https://randomapi.com/api/3ba71dfdc0144abba3452e039f210123';
     const { data: { results } } = await axios.get(URL);
     const [randomTestData] = results;
     const { value, correctFunction } = randomTestData;
 
-    expect(countEvenNumbersWithin(value)).toStrictEqual(correctFunction);
+    expect(evenNumbersWithin(value)).toStrictEqual(correctFunction);
 });
